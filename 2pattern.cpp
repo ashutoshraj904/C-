3d5b73c5e8d85e37689f833209d30bdfedb5e21e
Ashutosh Kumar <72262486+ashutoshraj904@users.noreply.github.com>
2023-03-21 21:49:00 +0530

    Update 2pattern.cpp

--- 2pattern.cpp
+++ 2pattern.cpp
@@ -1,20 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n,a=1;
-    int i=1;
+    int n,i=1;
     cin>>n;
-    while (i<=n) //column
-    {   
-       int j=1; //row
-        while (j<=n)
+    while (i<=n)
+    {
+        int j=1;
+        while (j<=1)
         {
-            cout<<a;
+            cout<<i;
             j++;
+
         }
         cout<<endl;
-        a++;
         i++;
-        }
-}
+    }
     
+}
